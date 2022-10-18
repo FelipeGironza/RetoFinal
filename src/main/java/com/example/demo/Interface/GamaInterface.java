b725fa53cf4a8898c63591a35807eb59c035aa7e
Felipe <felipegironzagamboa153@gmail.com>
2022-10-17 22:24:11 -0500

    first commit

---
+++ src/main/java/com/example/demo/Interface/GamaInterface.java
@@ -0,0 +1,9 @@
+package com.example.demo.Interface;
+
+import com.example.demo.Modelo.Gama;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GamaInterface extends CrudRepository <Gama, Integer>{
+    
+}
+
